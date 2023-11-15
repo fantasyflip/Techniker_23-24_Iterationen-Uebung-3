c05885ae524814d157e01444280e3523a9b46850
Philipp Katzenberger <katzenberger.ph+github@gmail.com>
2023-11-15 08:56:31 +0100

    feat(main): solution for task1

--- main.c
+++ main.c
@@ -1,7 +1,19 @@
 #include <stdio.h>
 
+
+void task1() {
+    int result = 0;
+
+    for(int i = 1; i <= 100; i++){
+        result = result + i;
+    }
+
+    printf("Die Summe aus allen Zahlen von 0-100 ist %d.\n\n", result);
+}
+
 int main()
 {
-    printf("Hello World!\n");
+    printf("Aufgabe 1:\n\n");
+    task1();
     return 0;
 }
